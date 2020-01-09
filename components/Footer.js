c8061ff24c7353673dc7ebebae0a6828e9c3e062
Martin Røed <msroed@gmail.com>
2020-01-09 23:29:34 +0100

    feat(notifications): add notification when there's a new upload! (#22)
    * feat(notifications): add notification when there's a new upload!
    
    * fix
    
    * .

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Layout, Tag } from 'antd';
+import { Layout, Tag, notification } from 'antd';
 import { getFirestore } from '../utils/firebase';
 
 const { Footer: AntFooter } = Layout;
@@ -12,6 +12,28 @@ export default function Footer(props) {
       .orderBy('uploadedAtMillis', 'desc')
       .limit(5)
       .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(function(change) {
+          if (change.type === 'added' || change.type === 'modified') {
+            // Assume non-first run
+            const d = change.doc.data();
+            if (d.uploadedAtMillis > Date.now() - 10 * 1000) {
+              const [_, year, season, day] = d.id.split('-');
+              notification.success({
+                message: `New upload from ${d.farmName}`,
+                description: `Day ${day} in ${season}, year ${year}, click to see it!`,
+                onClick: () => {
+                  window.location.href = window.location.href = `${
+                    window.location.href.split('?')[0]
+                  }?id=${d.id}`;
+                },
+                duration: 10,
+                style: {
+                  cursor: 'pointer',
+                },
+              });
+            }
+          }
+        });
         if (!snapshot.empty) {
           const saveGames = [];
           snapshot.forEach(doc => {
