f22cce342e0fa7c0662c4ae9fb9258f27a9e1c11
Martin <msroed@gmail.com>
2020-01-10 00:44:50 +0100

    capitalize all search queries to stardewwiki

--- pages/api/wikisearch.js
+++ pages/api/wikisearch.js
@@ -2,14 +2,24 @@ import axios from 'axios';
 
 const cache = {};
 
+const capitalize = s => {
+  if (typeof s !== 'string') return '';
+  return s
+    .split(' ')
+    .map(n => n.charAt(0).toUpperCase() + n.slice(1))
+    .join(' ');
+};
+
 export default async function wikisearch(req, res) {
-  const { q } = req.query;
+  const q = capitalize(req.query.q);
   if (cache[q]) {
     return res.json({ results: cache[q] });
   }
   try {
     const data = await axios.get(
-      `https://stardewvalleywiki.com/mediawiki/api.php?action=opensearch&format=json&search=${q}&namespace=0&limit=10&suggest=`
+      `https://stardewvalleywiki.com/mediawiki/api.php?action=opensearch&format=json&search=${encodeURIComponent(
+        q
+      )}&namespace=0&limit=10&suggest=`
     );
     if (data.data && data.data.length) {
       const [_, labels, __, urls] = data.data;
