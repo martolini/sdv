a8daf871ca3bb9a2e95462bebb770b005ab04b94
Linn Kristin Stokvik <linn@gridlabs.no>
2020-10-04 14:07:45 +0200

    Revert "fix: 🐛 Add error reporting"
    This reverts commit 7815c8efe9e66795b3122fb87eb1cc0ee0f94d79.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 // next.config.js
 const withCSS = require('@zeit/next-css');
-const withSourceMaps = require('@zeit/next-source-maps');
-const SentryCliPlugin = require('@sentry/webpack-plugin');
 
-module.exports = withSourceMaps(
-  withCSS({
-    webpack(config) {
-      const plugins = config.plugins || [];
-      if (process.env.CI === 'true' || process.env.NODE_ENV === 'production') {
-        plugins.push(
-          new SentryCliPlugin({
-            include: '.',
-            ignoreFile: '.sentrycliignore',
-            ignore: ['node_modules', 'next.config.js'],
-          })
-        );
-      }
-      config.plugins = plugins;
-      return config;
-    },
-  })
-);
+module.exports = withCSS({
+  /* config options here */
+});
--- utils/sentry.js
+++
@@ -1,5 +0,0 @@
-import * as Sentry from '@sentry/browser';
-
-export default Sentry.init({
-  dsn: 'https://07ee10176abc4e329de9387d92aef610@sentry.io/1888377',
-});
