6c4dbe0b756f696598194966ea3fda41196eba79
Linn Kristin Stokvik <linn@gridlabs.no>
2021-04-01 20:01:10 +0200

    fix: static files storybook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
 
 function MyApp({ Component, pageProps }) {
