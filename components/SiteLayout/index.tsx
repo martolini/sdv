7030bfa080052f414043ab0508333142a09aa787
Martin Roed <martin.roed@cognite.com>
2021-04-02 23:46:22 +0200

    site layout

--- components/SiteLayout/index.tsx
+++ components/SiteLayout/index.tsx
@@ -1,9 +1,17 @@
 import Header from 'components/Header';
 import { Pane } from 'evergreen-ui';
+import Head from 'next/head';
 
 const SiteLayout: React.FC = ({ children }) => {
   return (
     <Pane padding={5}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Header />
       <Pane marginX={16} marginY={24}>
         {children}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,11 +32,6 @@ export default class MyDocument extends Document<Props> {
       <Html>
         <Head>
           <style dangerouslySetInnerHTML={{ __html: css }} />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
-            rel="stylesheet"
-          />
         </Head>
 
         <body>
