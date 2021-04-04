8ca9d4a85634daae290df6537a8f762145053577
Martin Roed <martin.roed@cognite.com>
2021-04-04 21:34:49 +0200

    new bundle sizes

--- components/BundlesTable/index.tsx
+++ components/BundlesTable/index.tsx
@@ -1,19 +1,71 @@
-import { Badge, Pane, StarIcon, Table } from 'evergreen-ui';
+import {
+  Pane,
+  StarIcon,
+  Text,
+  Table,
+  TextTableCellProps,
+  TextTableHeaderCellProps,
+  useTheme,
+  TextProps,
+} from 'evergreen-ui';
+import { useMemo } from 'react';
 import { Bundle } from 'typings/stardew';
 
 type BundlesTableProps = {
   bundleInfo: Bundle[];
 };
 
+const CustomTextCell: React.FC<TextTableCellProps> = ({
+  children,
+  ...props
+}) => (
+  <Table.TextCell
+    textProps={{ fontSize: '1rem', letterSpacing: '0.3px' }}
+    {...props}
+  >
+    {children}
+  </Table.TextCell>
+);
+
+const qualityToColor = (quality) => {
+  switch (quality) {
+    case 1:
+      return 'silver';
+    case 2:
+      return 'gold';
+    case 3:
+      return 'purple';
+    default:
+      throw new Error();
+  }
+};
+
+const CustomHeaderCell: React.FC<TextTableHeaderCellProps> = ({
+  children,
+  ...props
+}) => (
+  <Table.TextHeaderCell textProps={{ fontSize: '1rem' }} {...props}>
+    {children}
+  </Table.TextHeaderCell>
+);
+
 export default function BundlesTable({ bundleInfo }: BundlesTableProps) {
+  const theme = useTheme();
+  const extraGreenProps: Partial<TextProps> = useMemo(
+    () => ({
+      backgroundColor: theme.palette.green.light,
+      color: theme.palette.green.dark,
+    }),
+    [theme]
+  );
   return (
     <Table width="100%">
       <Table.Head>
-        <Table.TextHeaderCell>Room name</Table.TextHeaderCell>
-        <Table.TextHeaderCell>Bundle name</Table.TextHeaderCell>
-        <Table.TextHeaderCell>Reward</Table.TextHeaderCell>
-        <Table.TextHeaderCell># of missing</Table.TextHeaderCell>
-        <Table.TextHeaderCell flexBasis={580}>Missing</Table.TextHeaderCell>
+        <CustomHeaderCell>Room name</CustomHeaderCell>
+        <CustomHeaderCell>Bundle name</CustomHeaderCell>
+        <CustomHeaderCell>Reward</CustomHeaderCell>
+        <CustomHeaderCell># of missing</CustomHeaderCell>
+        <CustomHeaderCell flexBasis={580}>Missing</CustomHeaderCell>
       </Table.Head>
       <Table.Body height={520}>
         {bundleInfo &&
@@ -21,38 +73,52 @@ export default function BundlesTable({ bundleInfo }: BundlesTableProps) {
             .filter((b) => b.missingIngredients.length > 0 && b.id !== '36')
             .map((bundle) => {
               return (
-                <Table.Row key={bundle.id} isSelectable>
-                  <Table.TextCell>{bundle.roomName}</Table.TextCell>
-                  <Table.TextCell>{bundle.bundleName}</Table.TextCell>
-                  <Table.TextCell>
+                <Table.Row
+                  key={bundle.id}
+                  marginY={5}
+                  height="auto"
+                  padding={5}
+                >
+                  <CustomTextCell>{bundle.roomName}</CustomTextCell>
+                  <CustomTextCell>{bundle.bundleName}</CustomTextCell>
+                  <CustomTextCell>
                     {bundle.reward.name} ({bundle.reward.stack})
-                  </Table.TextCell>
-                  <Table.TextCell isNumber>{bundle.nMissing}</Table.TextCell>
-                  <Table.TextCell flexBasis={580}>
+                  </CustomTextCell>
+                  <CustomTextCell isNumber>{bundle.nMissing}</CustomTextCell>
+                  <CustomTextCell flexBasis={580}>
                     <Pane display="flex" flexWrap="wrap">
                       {bundle.missingIngredients
-                        .filter((elem) => elem.name)
+                        .filter((e) => e.name)
+                        .sort(
+                          (a, b) =>
+                            +b.deliverableInBundle - +a.deliverableInBundle
+                        )
                         .map((elem) => (
-                          <Badge
-                            marginBottom={3}
+                          <Text
+                            size={500}
                             key={elem.itemId}
-                            color={
-                              elem.deliverableInBundle ? 'green' : 'neutral'
-                            }
-                            marginRight={8}
+                            marginX={5}
+                            marginY={2}
+                            paddingX={8}
+                            paddingY={2}
+                            backgroundColor={theme.scales.neutral.N3}
+                            borderRadius={6}
+                            {...(elem.deliverableInBundle
+                              ? extraGreenProps
+                              : {})}
                           >
-                            {elem.name}
+                            {elem.name} ({elem.stack}
                             {elem.quality > 0 && (
                               <StarIcon
-                                size={9}
-                                color={elem.quality === 1 ? 'silver' : 'gold'}
+                                size={10}
+                                color={qualityToColor(elem.quality)}
                               />
                             )}
-                            ({elem.stack})
-                          </Badge>
+                            )
+                          </Text>
                         ))}
                     </Pane>
-                  </Table.TextCell>
+                  </CustomTextCell>
                 </Table.Row>
               );
             })}
