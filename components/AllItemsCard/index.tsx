826052c6a5fc319143c129445f366d6f9127d2b9
Martin Roed <martin.roed@cognite.com>
2021-04-25 11:46:32 +0200

    feat: 🎸 Add upload listener

--- components/AllItemsCard/index.tsx
+++ components/AllItemsCard/index.tsx
@@ -5,78 +5,84 @@ import React, { useMemo, useState } from 'react';
 import { GiLockedChest } from '@react-icons/all-files/gi/GiLockedChest';
 import { chain, values } from 'lodash';
 import { qualityToColor } from 'utils/stardew-helpers';
+import { Item } from 'typings/stardew';
+
+type KeyedItem = Item & { key: string };
 
 const AllItemsCard: React.FC = () => {
   const { parsedGame } = useParsedGame();
   const [filterValue, setFilterValue] = useState('');
 
-  const allItems = useMemo(() => {
+  const allItems: KeyedItem[] = useMemo(() => {
     return values(
       chain(parsedGame.items)
         .groupBy(
           (item) =>
             `${item.itemId}_${item.quality}_${item.chestColor || item.player}`
         )
-        .reduce((p, current) => [
+        .reduce((p, current, key) => [
           ...p,
           {
+            key,
             ...current[0],
             stack: current.reduce((p, c) => p + c.stack, 0),
           },
         ])
-        .value()
-    ).flat();
+        .value() as KeyedItem[]
+    )
+      .flat()
+      .filter((item) => item.key !== undefined);
   }, [parsedGame]);
 
+  const tableRows = useMemo(
+    () =>
+      allItems
+        .filter((i) =>
+          filterValue ? new RegExp(filterValue, 'i').test(i.name) : true
+        )
+        .map(({ name, stack, chestColor, player, quality, key }) => (
+          <Table.Row key={key}>
+            <Table.TextCell textProps={{ fontSize: '1.1rem' }}>
+              {name}
+              {quality > 0 && (
+                <StarIcon size={12} color={qualityToColor(quality)} />
+              )}
+              {stack && ` (${stack})`}
+            </Table.TextCell>
+            <Table.TextCell
+              textProps={{ fontSize: '1.1rem' }}
+              isNumber
+              flexBasis={70}
+              flexShrink={0}
+              flexGrow={0}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              textAlign="center"
+            >
+              {chestColor ? (
+                <GiLockedChest color={`#${chestColor}`} />
+              ) : (
+                <Avatar
+                  name={player || ''}
+                  size={35}
+                  getInitials={(name) => name.substring(0, 2).toUpperCase()}
+                />
+              )}
+            </Table.TextCell>
+          </Table.Row>
+        )),
+    [allItems, filterValue]
+  );
   return (
     <Pane flexDirection="column" height={430} width="100%">
       <CardTitle>Search in inventory</CardTitle>
       <hr color="#e6e6e6" />
       <Table width="100%">
         <Table.Head>
-          <Table.SearchHeaderCell
-            value={filterValue}
-            onChange={(value) => setFilterValue(value)}
-          />
+          <Table.SearchHeaderCell onChange={(value) => setFilterValue(value)} />
         </Table.Head>
-        <Table.Body maxHeight={360}>
-          {allItems
-            .filter((i) =>
-              filterValue ? new RegExp(filterValue, 'i').test(i.name) : true
-            )
-            .map(({ name, stack, chestColor, player, quality }, i) => (
-              <Table.Row key={i}>
-                <Table.TextCell textProps={{ fontSize: '1.1rem' }}>
-                  {name}
-                  {quality > 0 && (
-                    <StarIcon size={12} color={qualityToColor(quality)} />
-                  )}
-                  {stack && ` (${stack})`}
-                </Table.TextCell>
-                <Table.TextCell
-                  textProps={{ fontSize: '1.1rem' }}
-                  isNumber
-                  flexBasis={70}
-                  flexShrink={0}
-                  flexGrow={0}
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  textAlign="center"
-                >
-                  {chestColor ? (
-                    <GiLockedChest color={`#${chestColor}`} />
-                  ) : (
-                    <Avatar
-                      name={player || ''}
-                      size={35}
-                      getInitials={(name) => name.substring(0, 2).toUpperCase()}
-                    />
-                  )}
-                </Table.TextCell>
-              </Table.Row>
-            ))}
-        </Table.Body>
+        <Table.Body maxHeight={360}>{tableRows}</Table.Body>
       </Table>
     </Pane>
   );
