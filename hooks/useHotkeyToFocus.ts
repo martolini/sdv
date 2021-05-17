d2f2802b64b77816aae135c6da6bf2eaba3442f7
Martin Roed <msroed@gmail.com>
2021-05-17 16:29:35 +0200

    fix: 🐛 Check that document is available in useHotkeysToFocus

--- hooks/useHotkeyToFocus.ts
+++ hooks/useHotkeyToFocus.ts
@@ -5,20 +5,25 @@ export default function useHotkeyToFocus<T extends MutableRefObject<any>>(
   keys: string[]
 ): T {
   useEffect(() => {
-    const keyHandler = (e) => {
-      if (keys.includes(e.key) && document.activeElement.nodeName !== 'INPUT') {
-        inputRef.current.focus();
-      }
-      // If escape key is hit and the input field is focused
-      if (e.key === 'Escape' && document.activeElement === inputRef.current) {
-        inputRef.current.blur();
-      }
-    };
-    window.addEventListener('keyup', keyHandler);
-    // Remove event listeners on cleanup
-    return () => {
-      window.removeEventListener('keyup', keyHandler);
-    };
+    if (document) {
+      const keyHandler = (e) => {
+        if (
+          keys.includes(e.key) &&
+          document.activeElement.nodeName !== 'INPUT'
+        ) {
+          inputRef.current.focus();
+        }
+        // If escape key is hit and the input field is focused
+        if (e.key === 'Escape' && document.activeElement === inputRef.current) {
+          inputRef.current.blur();
+        }
+      };
+      window.addEventListener('keyup', keyHandler);
+      // Remove event listeners on cleanup
+      return () => {
+        window.removeEventListener('keyup', keyHandler);
+      };
+    }
   }, [inputRef, keys]);
   return inputRef;
 }
