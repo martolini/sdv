11e07b2793371ea38b3d2a1c27e6480f023311a7
Martin Roed <msroed@gmail.com>
2021-05-17 15:53:21 +0200

    popover

--- components/WikiSearch/index.tsx
+++ components/WikiSearch/index.tsx
@@ -8,20 +8,20 @@ import React, {
 import useSearch from './useSearch';
 import { useDebounce } from 'use-debounce';
 import Suggestion from './Suggestion';
-import { Link, Pane, Popover, SearchInput } from 'evergreen-ui';
+import { Pane, Popover, SearchInput } from 'evergreen-ui';
 import { useParsedGame } from 'hooks/useParsedGame';
-import useHotkeyToFocus from 'hooks/useHotkeyToFocus';
 
 export default function WikiSearch() {
   const [inputValue, setInputValue] = useState('');
   const [focusedResult, setFocusedResult] = useState(0);
   const searchIndex = useSearch();
   const { parsedGame } = useParsedGame();
+  const searchRef = useRef<any>();
   const getSuggestions = useCallback(
     (query) => {
       if (query && query.trim().length === 0) return [];
       const suggestions = searchIndex.search(query.trim(), {
-        limit: 3,
+        limit: 5,
       });
       return suggestions;
     },
@@ -41,25 +41,34 @@ export default function WikiSearch() {
           padding: 0,
           margin: 0,
         }}
+        onClick={(e) => {
+          e.preventDefault();
+          const { item: { href = null } = {} } =
+            suggestions[focusedResult] || {};
+          if (href) {
+            window.open(`https://stardewvalleywiki.com${href}`, '_blank');
+          }
+          searchRef.current.focus();
+        }}
       >
         {suggestions.map((sugg, i) => (
-          <li key={sugg.refIndex}>
-            <Link
-              href={`https://stardewvalleywiki.com${sugg.item.href}`}
-              target="_blank"
-            >
-              <Suggestion
-                key={sugg.refIndex}
-                item={sugg.item}
-                focused={focusedResult === i}
-                onFocused={() => setFocusedResult(i)}
-              />
-            </Link>
+          <li
+            key={sugg.refIndex}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <Suggestion
+              key={sugg.refIndex}
+              item={sugg.item}
+              focused={focusedResult === i}
+              onFocused={() => setFocusedResult(i)}
+            />
           </li>
         ))}
       </ul>
     );
-  }, [suggestions, focusedResult, parsedGame]);
+  }, [suggestions, focusedResult, parsedGame, searchRef]);
 
   const onChange = useCallback((e) => {
     setInputValue(e.target.value);
@@ -67,7 +76,6 @@ export default function WikiSearch() {
 
   const onKeyPress = useCallback(
     (e) => {
-      console.log('pressed');
       switch (e.key) {
         case 'ArrowUp': {
           setFocusedResult((prev) => {
@@ -77,7 +85,6 @@ export default function WikiSearch() {
           break;
         }
         case 'ArrowDown': {
-          console.log('ummm');
           setFocusedResult((prev) => {
             return Math.min(prev + 1, suggestions.length - 1);
           });
@@ -90,7 +97,6 @@ export default function WikiSearch() {
           if (href) {
             window.open(`https://stardewvalleywiki.com${href}`, '_blank');
           }
-          e.preventDefault();
           break;
         }
         default: {
@@ -100,8 +106,6 @@ export default function WikiSearch() {
     },
     [setFocusedResult, suggestions, focusedResult]
   );
-  const searchRef = useRef<any>();
-  useHotkeyToFocus(searchRef, ['s']);
   useEffect(() => {
     setFocusedResult(0);
   }, [suggestions]);
@@ -109,20 +113,20 @@ export default function WikiSearch() {
   return (
     <Popover
       content={searchResults || []}
-      isShown={suggestions.length > 0 || true}
-      minWidth="30%"
+      isShown={suggestions.length > 0}
+      minWidth="40%"
       position="bottom"
     >
       <Pane width="100%">
         <SearchInput
           placeholder="search wiki"
           width="100%"
+          onKeyDown={onKeyPress}
           value={inputValue}
           onBlur={() => {
             // setInputValue('');
           }}
           onChange={onChange}
-          onKeyDown={onKeyPress}
           ref={searchRef}
           style={{
             fontSize: '1rem',
